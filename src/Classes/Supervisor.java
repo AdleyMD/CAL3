1ec51327fa86d53fbd7c77989c85d7d860ebb3ed
Yaxeb <chejof1710@gmail.com>
2020-05-08 13:44:25 +0200

    supervisor creado

--- src/Classes/Supervisor.java
+++ src/Classes/Supervisor.java
@@ -1,33 +1,37 @@
 package Classes;
 
 /**
- * @author andro
+ * @author CHAD (Copper Heroes Andrei & Darius)
  * @version 1.0
  * @created 08-may.-2020 12:16:09
  */
 public class Supervisor {
 
-	private int id;
+    private int id;
 
-	public Supervisor(){
+    public Supervisor(int id) {
+        this.id = id;
+    }
 
-	}
+    /**
+     *
+     * @param min
+     * @param max
+     * @param age
+     * @param user
+     * @return 
+     */
+    public boolean checkAge(int min, int max, int age) {
+        return (min <= age && age <= max);
+    }
 
-	/**
-	 * 
-	 * @param min
-	 * @param max
-	 * @param user
-	 */
-	public boolean checkAge(int min, int max, User user){
-		return false;
-	}
-
-	/**
-	 * 
-	 * @param user
-	 */
-	public boolean checkCompanion(User user){
-		return false;
-	}
+    /**
+     *
+     * @param user
+     * @return 
+     */
+    public boolean checkCompanion(User user) {
+        return (user.getAge() <= 10);
+    }
+    
 }//end Supervisor
