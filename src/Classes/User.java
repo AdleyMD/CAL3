b802914f6a32de01d42491237a0eda2a8a9bd78b
AdleyMD <android2026@hotmail.com>
2020-05-08 13:10:10 +0200

    Files

--- src/Classes/User.java
+++ src/Classes/User.java
@@ -5,21 +5,43 @@ package Classes;
  * @version 1.0
  * @created 08-may.-2020 12:16:10
  */
-public class User {
+public class User extends Thread {
+    
+    private WaterPark park;
+    private int actCounter;
+    private int age;
+    private int id;
+    private int idCompanion;
 
-	private int actCounter;
-	private int age;
-	private int id;
-	private int idCompanion;
+    public User(){
+        actCounter = (int) (5 + (10 * Math.random()));
+        age = (int) (5 + (10 * Math.random()));
+    }
+    
+    @Override
+    public void run() {
+        
+    }
+    
+    public void subActCounter(){
+        actCounter--;
+    }
 
-	public User(){
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public int getUserId() {
+        return id;
+    }
 
-	}
-
-	public void finalize() throws Throwable {
-
-	}
-	public void subActCounter(){
-
-	}
+    public int getIdCompanion() {
+        return idCompanion;
+    }
+        
+        
 }//end User
