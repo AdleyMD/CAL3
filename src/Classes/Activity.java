0f1af5f925021c1f1a4c07c1c1cf2ec4971b62a8
Yaxeb <chejof1710@gmail.com>
2020-05-08 15:57:39 +0200

    enter function, Children pool & Activity

--- src/Classes/Activity.java
+++ src/Classes/Activity.java
@@ -3,25 +3,24 @@ package Classes;
 import java.util.ArrayList;
 
 /**
- * @author andro
+ * @author CHAD (Copper Heroes Andrei & Darius)
  * @version 1.0
  * @created 08-may.-2020 12:16:03
  */
-public class Activity {
+public abstract class Activity {
 
-private int maxUsers;
-private Supervisor supervisor;
-public Queue m_Queue;
-public Supervisor m_Supervisor;
-	private int maxUsers;
-	private Supervisor supervisor;
-        private Queue queue;
-        private ArrayList<User> inside;
+    public int maxUsers;
+    public Supervisor supervisor;
+    public Queue queue;
+    public ArrayList<User> inside;
 
-
-public Activity(int maxUsers){
-    this.maxUsers = maxUsers;
-}
-
-	
+    public Activity(int maxUsers, Supervisor supervisor, Queue queue, ArrayList<User> inside) {
+        this.maxUsers = maxUsers;
+        this.supervisor = supervisor;
+        this.queue = queue;
+        this.inside = inside;        
+    }
+    
+    public abstract void enter();
+    
 }//end Activity
--- src/Classes/ChildrenPool.java
+++ src/Classes/ChildrenPool.java
@@ -1,14 +1,42 @@
 package Classes;
 
+import java.util.ArrayList;
+
 /**
- * @author andro
+ * @author CHAD (Copper Heroes Andrei & Darius)
  * @version 1.0
  * @created 08-may.-2020 12:16:06
  */
 public class ChildrenPool extends Activity {
 
-	public ChildrenPool(){
+    private boolean needsCompanion;
+    private int capacity;
 
-	}
+    public ChildrenPool(int maxUsers, Supervisor supervisor, Queue queue, ArrayList<User> inside) {
+        super(maxUsers, supervisor, queue, inside);
+    }
+
+    @Override
+    public void enter() {
+
+        if (capacity != maxUsers) {
+            User visitor = queue.dequeue();
+            int visitorAge = visitor.getAge();
+            if (supervisor.checkAge(1, 5, visitorAge)) {
+                needsCompanion = true;
+            } else if (supervisor.checkAge(6, 10, visitorAge)) {
+                needsCompanion = false;
+            }
+            if (needsCompanion) {
+                capacity += 1;
+            }
+            capacity += 1;
+        }
+    }
+    
+    
+    public void leave() {
+        
+    }
 
 }//end ChildrenPool
