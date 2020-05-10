92b27cd0ca503f4c9085359620b9866f70760989
Yaxeb <chejof1710@gmail.com>
2020-05-11 01:16:58 +0200

    Children.java working properly, not displayed

--- src/Classes/ChildrenPool.java
+++ src/Classes/ChildrenPool.java
@@ -18,28 +18,41 @@ public class ChildrenPool extends Activity {
 
     @Override
     public boolean canEnter(User user) {
-        return ((getCurCapacity() < getMaxUsers() - 1 && user.getAge() < 5) || (getCurCapacity() < getMaxUsers()) && user.hasCompanion());
+        System.out.println("current capacity : " + getCurCapacity());
+        System.out.println("maxUsers : " + getMaxUsers());
+
+        return ((getCurCapacity() < getMaxUsers() - 1 && user.hasCompanion())
+                || (getCurCapacity() < getMaxUsers()));
+
     }
 
     @Override
     public void enter(User user) {
+
         getLock().lock();
-        getSupervisor().setUserToCheck(user);
         getExecutor().execute(getSupervisor());
+        getSupervisor().setUserToCheck(user);
         getQueue().enqueue(user);
+
         try {
+            // compruebo la edad aqui blabla...
             while (!canEnter(user)) {
+                System.out.println("waiting lmao");
                 getActFull().await();
             }
+            getQueue().dequeue();
+            System.out.println("supervisorsaidno: " + supervisorSaidNo(user));
             if (supervisorSaidNo(user)) {
+                System.out.println("supervisor said no");
                 return;
             }
 
-            if (user.hasCompanion()) {
+            if (user.hasCompanion() && user.getAge() <= 5) {
+                System.out.println("i have a companion lmao im smol");
                 addCurCapacity(1);
             }
             addCurCapacity(1);
-            getQueue().dequeue();
+
         } catch (InterruptedException ex) {
             Logger.getLogger(ChildrenPool.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
@@ -50,14 +63,17 @@ public class ChildrenPool extends Activity {
 
     @Override
     public void use(User user) {
-        if (supervisorSaidNo(user)) {
-            return;
+        if (!supervisorSaidNo(user)) {
+            customSleep(1000, 3000);
         }
-        customSleep(1000, 3000);
+        return;
     }
 
     @Override
     public void leave(User user) {
+        if (supervisorSaidNo(user)) {
+            return;
+        }
         try {
             getLock().lock();
             getInside().remove(user);
